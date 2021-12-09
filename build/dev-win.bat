c9c5dde6c6149fd3a857e0025d0ef80806fd946b
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-12-09 20:46:57 +0000

    build: Add dev-win build command.

---
+++ build/dev-win.bat
@@ -0,0 +1,1 @@
+cargo watch --use-shell=cmd -- PowerShell (cargo build --features gmcl --release); (Copy-Item target/release/gmcl_sysinfo_win64.dll -Destination H:\Steam\steamapps\common\GarrysMod\garrysmod\lua\bin)
