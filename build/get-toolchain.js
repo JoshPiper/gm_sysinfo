04731d0dc04eca164a10208f87715b68612bc984
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-12-19 18:17:44 +0000

    build: Add build workflow and additional toolchains.

---
+++ build/get-toolchain.js
@@ -0,0 +1,38 @@
+
+console.log("Loading Toolchain Data")
+
+const {PLATFORM: platform, ARCH: arch, REALM: realm} = process.env
+const modname = process.env["MODULE"] ?? "sysinfo"
+
+console.log(`Configuring ${modname} for ${platform}${arch} on ${realm}`)
+
+const archs = {
+	["32"]: "i686",
+	["64"]: "x86_64"
+}
+
+const platforms = {
+	win: "pc-windows-msvc",
+	osx: "apple-darwin",
+	linux: "unknown-linux-gnu"
+}
+
+const suffixes = {
+	linux32: "linux"
+}
+
+const features = {
+	cl: 'gmcl'
+}
+
+const toolchain = `${archs[arch]}-${platforms[platform]}`
+console.log(`::set-output name=toolchain::${toolchain}`)
+
+const file = `gm${realm}_${modname}_${suffixes[`${platform}${arch}`] ?? `${platform}${arch}`}.dll`
+console.log(`::set-output name=file::${file}`)
+
+let feature = features[realm]
+if (feature !== ""){
+	feature = `--features ${feature}`
+}
+console.log(`::set-output name=features::${feature}`)
